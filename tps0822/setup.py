598bce3941841b57e268aad102fae0ff77ac3d18
root <broughton.graham@gmail.com>
2022-08-01 13:33:03 +0000

    organized structure

---
+++ tps0822/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tps0822',
+    version='0.1'
+    )
---
+++ tps0822/tps0822/config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+base = Path('content/drive/MyDrive/Kaggle_TPS_0822')
+data_dir = base / 'data/'
+train_dir = base / 'data/train.csv'
+sub_dir = base / 'submissions/'
